c411b7dff43a8269fcf11a009e5df996de297ef4
Sumit <108573409+CrutoSJ@users.noreply.github.com>
2023-06-07 22:59:26 +0530

    Create add_two.py

---
+++ #Leetcode(Python)/add_two.py
@@ -0,0 +1,12 @@
+# Question:-
+
+# Gives sum of two no.
+# This is a test .py file to check the git / github usage 
+
+# Solution:-
+
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+
+ans = a+b
+print("The sum of numbers: ",ans)
