7f5ff9e7cf26473de42c74c5d2e3dcb63f3471b3
Sumit <jha.07sumit2003@gmail.com>
2023-08-24 22:31:56 +0530

    Added Count pairs whose Sum is less than target

---
+++ #Leetcode(C++)/Count_Pairs_Whose_Sum_is_Less_than_Target.cpp
@@ -0,0 +1,47 @@
+// #Question:-
+
+// Given a 0-indexed integer array nums of length n and an integer target, return the number of pairs (i, j) where 0 <= i < j < n and nums[i] + nums[j] < target.
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int countPairs(vector<int>& nums, int target) {
+        int cnt = 0 ;
+        int n = nums.size();
+        multiset<int>st ;
+        for(int i = 0 ; i < n ;i++){
+            auto pos = st.lower_bound(target-nums[i]);
+            cnt += distance(st.begin(),pos);
+            st.insert(nums[i]);
+        }
+        return cnt ;
+    }
+};
+
+int main(){
+    int t;
+    cout << "Enter the no. of test cases: ";
+    cin >> t;
+    while(t--){
+        int n, target;
+        cout << "Enter the length of array and target: ";
+        cin >> n >> target;
+
+        vector<int> nums(n);
+        cout << "Enter array elements: ";
+        for (int i = 0; i < n; i++) {
+            cin >> nums[i];
+        }
+
+        Solution solution;
+        int result = solution.countPairs(nums, target);
+        cout << "Number of pairs where sum is less than target: " << result << endl;
+    }
+
+    return 0;
+
+}
