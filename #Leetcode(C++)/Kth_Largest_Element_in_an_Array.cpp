a35901821e85477f46e9a3c0e9e69a93d1a41ac9
Sumit <jha.07sumit2003@gmail.com>
2023-10-09 19:40:34 +0530

    Kth Largest Element in an Array

---
+++ #Leetcode(C++)/Kth_Largest_Element_in_an_Array.cpp
@@ -0,0 +1,47 @@
+// #Question:-
+
+// Given an integer array nums and an integer k, return the kth largest element in the array.
+
+// Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+// Can you solve it without sorting?
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()-k];
+    }
+};
+
+int main(){
+    int t;
+    cout << "Enter no. of test cases: ";
+    cin >> t;
+
+    while(t--) {
+        int n, k;
+        cout << "Enter the number of elements in the array: ";
+        cin >> n;
+        
+        vector<int> nums(n);
+        cout << "Enter the elements of the array: ";
+        for (int i = 0; i < n; ++i) {
+            cin >> nums[i];
+        }
+
+        cout << "Enter k: ";
+        cin >> k;
+
+        Solution obj;
+        int result = obj.findKthLargest(nums, k);
+        cout << "The kth largest element is: " << result << endl;
+    }
+
+    return 0;
+}
