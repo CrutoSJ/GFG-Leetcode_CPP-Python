1dc1f2820151e34b5fd208e3357eafab2837d50d
Sumit <jha.07sumit2003@gmail.com>
2024-03-17 20:27:58 +0530

    1 liner C++ STl Code

---
+++ #Leetcode(C++)/LargestEle.cpp
@@ -0,0 +1,10 @@
+// #Question:-
+
+// Link-> https://www.codingninjas.com/studio/problems/largest-element-in-the-array-largest-element-in-the-array_5026279
+
+// #Solution:-
+
+#include <bits/stdc++.h> 
+int largestElement(vector<int> &arr, int n) {
+    return *max_element(arr.begin(), arr.end());
+}
