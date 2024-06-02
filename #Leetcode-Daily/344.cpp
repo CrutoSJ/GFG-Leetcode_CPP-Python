50bd62d3e91e17cf560661640d48e93e5cf836d3
Sumit <jha.07sumit2003@gmail.com>
2024-06-02 15:12:50 +0530

    Did this with a while loop & swap function

---
+++ #Leetcode-Daily/344.cpp
@@ -0,0 +1,21 @@
+// #Question:-
+
+// Link-> https://leetcode.com/problems/reverse-string/
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int r=s.size()-1;
+        int l=0;
+        while(l<=r){
+            swap(s[r],s[l]);
+            l++;
+            r--;
+        }
+    }
+};
