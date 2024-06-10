ce5939da4b1871457bebf78fe3961a83e3b2f0d5
Sumit <jha.07sumit2003@gmail.com>
2024-06-10 22:19:22 +0530

    Used the sort function

---
+++ #GFG-Daily/Nuts&BoltsProblem.cpp
@@ -0,0 +1,20 @@
+// #Question:-
+
+// Link-> https://www.geeksforgeeks.org/problems/nuts-and-bolts-problem0431/1
+
+// Date-> 10/06/24
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    void matchPairs(int n, char nuts[], char bolts[]) {
+        sort(nuts,nuts+n);
+        sort(bolts,bolts+n);
+        
+    }
+};
