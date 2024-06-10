ffb28f8ff083cdc0a909395639b28977522a6c1c
Sumit <jha.07sumit2003@gmail.com>
2024-06-10 22:37:30 +0530

    Used the sort function, Added the map Solution as well

--- #GFG-Daily/Nuts&BoltsProblem.cpp
+++ #GFG-Daily/Nuts&BoltsProblem.cpp
@@ -12,9 +12,36 @@ using namespace std;
 class Solution {
 public:
 
+    /*
+
+    ********** Did this using Sort Function ************
+
+    */
     void matchPairs(int n, char nuts[], char bolts[]) {
         sort(nuts,nuts+n);
         sort(bolts,bolts+n);
         
+    }
+
+    /*
+
+    ********** Did this using Ordered Map ************
+
+    */
+    void matchPairsTwo(int n, char nuts[], char bolts[]) {
+        map<char,int>order;
+        for(int i=0; i<n; i++){
+            order[nuts[i]]++;
+        }
+        
+        int i=0;
+        for(auto it=order.begin(); it!=order.end(); ++it){
+            if(i<n && it->second>0){
+                nuts[i] = it->first;
+                bolts[i] = it->first;
+                i++;
+                it->second--;
+            }
+        }
     }
 };
