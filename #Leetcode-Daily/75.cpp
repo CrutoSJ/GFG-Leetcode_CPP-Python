98d6000c81ad18c96df188b76ca2fa9e97410df3
Sumit <jha.07sumit2003@gmail.com>
2024-06-12 11:32:50 +0530

    Did this question using 3 pointers - Dutch National Flag

---
+++ #Leetcode-Daily/75.cpp
@@ -0,0 +1,30 @@
+// #Question:-
+
+// Link-> https://leetcode.com/problems/sort-colors/
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i=0, j=0;
+        int k=nums.size()-1;
+        while(j<=k){
+            if(nums[j]==0){
+                swap(nums[i],nums[j]);
+                i++;
+                j++;
+            }
+            else if(nums[j]==1){
+                j++;
+            }
+            else if(nums[j]==2){
+                swap(nums[j], nums[k]);
+                k--;
+            }
+        }
+    }
+};
