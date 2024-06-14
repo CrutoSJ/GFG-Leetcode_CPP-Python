cde5c3ab56f918b7148f2f45a5ee4980e7a8bf56
Sumit <jha.07sumit2003@gmail.com>
2024-06-14 22:24:45 +0530

    Did this with the Brute Force Approach - O(n^3) Dammmm... need to optimize :( ..... Ps:- Updated it to O(n) :), increased 1 at a time for every duplicate formed & present already

--- #Leetcode-Daily/945.cpp
+++ #Leetcode-Daily/945.cpp
@@ -33,4 +33,18 @@ public:
         }
         return moves;
     }
+
+    //Optimised - O(n):-
+
+    int minIncrementForUnique2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int count=0;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]<=nums[i-1]){
+                count+=(nums[i-1]+1 -nums[i]);
+                nums[i]=nums[i-1]+1;
+            }
+        }
+        return count;
+    }
 };
