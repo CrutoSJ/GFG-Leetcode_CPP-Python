6fcbe1dff4ee5b4d8392ebcee6e1f0530985c0b4
Sumit <jha.07sumit2003@gmail.com>
2024-09-04 22:22:40 +0530

    Used math

---
+++ #GFG-Daily/NthStair(dist).cpp
@@ -0,0 +1,17 @@
+// #Question:-
+
+// Link-> https://www.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter5639/1
+
+// Date-> 04/09/24
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+	public:
+		int nthStair(int n){
+		    return 1+n/2;
+		}
+};
