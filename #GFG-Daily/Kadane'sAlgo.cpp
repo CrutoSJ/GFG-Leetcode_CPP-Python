a5b1ca82c1b3891b0ff20a65b9776886f46522f6
Sumit <jha.07sumit2003@gmail.com>
2024-09-06 23:15:01 +0530

    Initiated 2 variables & implemented Kadanes Algo

---
+++ #GFG-Daily/Kadane'sAlgo.cpp
@@ -0,0 +1,33 @@
+// #Question:-
+
+// Link-> https://www.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
+
+// Date-> 06/09/24
+
+// #Solution:-
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(vector<int> &arr) {
+        long long maxCnt = INT_MIN;
+        long long maxSum = 0;
+        
+        for(int i=0; i<arr.size(); i++){
+            maxSum += arr[i];
+            
+            if(maxCnt<maxSum){
+                maxCnt = maxSum;
+            }
+            
+            if(maxSum<0){
+                maxSum = 0;
+            }
+        }
+        
+        return maxCnt;
+    }
+};
