62744811e72fdcadccdfbe18d753516ea8a9c2aa
Sumit <jha.07sumit2003@gmail.com>
2024-09-09 18:55:50 +0530

    Did it with 3 pointers, low,mid,high & swap accordingly

---
+++ #GFG-Daily/Sort012.cpp
@@ -0,0 +1,30 @@
+// #Question:-
+
+// Link-> https://www.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+
+// Date-> 09/09/24
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        int n = arr.size();
+        int low=0, mid=0, high=n-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap(arr[low],arr[mid]);
+                low++,mid++;
+            } else if(arr[mid]==1){
+                mid++;
+            } else if(arr[mid]==2){
+                swap(arr[mid], arr[high]);
+                high--;
+            }
+        }
+        return ;
+    }
+};
