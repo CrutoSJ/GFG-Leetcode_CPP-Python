35549cbc3894d63c1c7f9cdd48b6c8609a5202ef
Sumit <jha.07sumit2003@gmail.com>
2024-09-13 23:00:40 +0530

    Did this using simple traversal and swapping the nodes

---
+++ #GFG-Daily/MirrorTree.cpp
@@ -0,0 +1,35 @@
+// #Question:-
+
+// Link-> https://www.geeksforgeeks.org/problems/mirror-tree/1
+
+// Date-> 13/09/24
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+class Solution {
+  public:
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node* node) {
+        if(node == NULL){
+            return ;
+        }
+        mirror(node->left);
+        mirror(node->right);
+        swap(node->left, node->right);
+    }
+};
