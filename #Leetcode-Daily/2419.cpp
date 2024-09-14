934dec9ee69754e231ef33fd4759bf0e94393988
Sumit <jha.07sumit2003@gmail.com>
2024-09-14 11:13:20 +0530

    Did this by simple observation

---
+++ #Leetcode-Daily/2419.cpp
@@ -0,0 +1,35 @@
+// #Question:-
+
+// Link-> https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/description/?envType=daily-question&envId=2024-09-14
+
+// Date-> 14/09/24
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int result, maxLen, maxEle = 0;
+        for(auto &num : nums){
+            if(num > maxEle){
+                maxEle = num; 
+                maxLen = 0;
+                result = 0;
+            }
+
+            if(maxEle == num){
+                maxLen++;
+            }
+
+            else{
+                maxLen = 0;
+            }
+
+            result = max(result, maxLen);
+        }
+        return result;
+    }
+};
