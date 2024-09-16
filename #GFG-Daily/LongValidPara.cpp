53fc8508d837f89c3a9ab209f199635135f53219
Sumit <jha.07sumit2003@gmail.com>
2024-09-16 23:20:32 +0530

    Did this question using stack, tracked the unmatched index and return maxLen

---
+++ #GFG-Daily/LongValidPara.cpp
@@ -0,0 +1,36 @@
+// #Question:-
+
+// Link-> https://www.geeksforgeeks.org/problems/longest-valid-parentheses5657/1
+
+// Date-> 16/09/24
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int maxLength(string& str) {
+        int n = str.length();
+        stack<int> st;
+        st.push(-1);
+        
+        int result = 0;
+        
+        for(int i=0; i<n; i++){
+            if(str[i] == '('){
+                st.push(i);
+            } else{
+                st.pop();
+                if(!st.empty()){
+                    result = max(result, i - st.top());
+                } else{
+                    st.push(i);
+                }
+            }
+        }
+        
+        return result;
+    }
+};
