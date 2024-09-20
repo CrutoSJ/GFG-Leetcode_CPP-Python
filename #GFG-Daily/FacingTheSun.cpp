9df00b85c4d38610f3830eb861977f905ec0ddf4
Sumit <jha.07sumit2003@gmail.com>
2024-09-20 14:30:50 +0530

    Did this by tracking limit variable and counting buildings greater than that, and updating limit to it

---
+++ #GFG-Daily/FacingTheSun.cpp
@@ -0,0 +1,28 @@
+// #Question:-
+
+// Link-> https://www.geeksforgeeks.org/problems/facing-the-sun2126/1
+
+// Date-> 20/09/24
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    // Returns count buildings that can see sunlight
+    int countBuildings(vector<int> &height) {
+        int count = 0;
+        int limit = INT_MIN;
+        
+        for(auto &x : height){
+            if(x>limit){
+                covscunt++;
+                limit=x;
+            }
+        }
+        
+        return count;
+    }
+};
