bc028f75843b942d9954edbc96d4ec7e5996b362
Sumit <jha.07sumit2003@gmail.com>
2024-09-21 22:17:46 +0530

    Did this question by recursion(DFS)

---
+++ #Leetcode-Daily/386.cpp
@@ -0,0 +1,41 @@
+// #Question:-
+
+// Link-> https://leetcode.com/problems/lexicographical-numbers/description/?envType=daily-question&envId=2024-09-21
+
+// Date-> 21/09/24
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void solve(int curr, int n, vector<int> &result){
+        if(curr>n){
+            return ;
+        }
+
+        result.push_back(curr);
+
+        for(int append=0; append<=9; append++){
+            int newNum = (curr*10) + append;
+
+            if(newNum>n){
+                return ;
+            }
+
+            solve(newNum, n, result);
+        }
+    }
+
+    vector<int> lexicalOrder(int n) {
+        vector<int> result;
+
+        for(int startNum = 1; startNum<=9; startNum++){
+            solve(startNum, n, result);
+        }
+
+        return result;
+    }
+};
