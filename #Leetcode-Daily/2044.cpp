35a60cfd62b68425f83edc0c79e94fca5adfbe2d
Sumit <jha.07sumit2003@gmail.com>
2024-10-18 21:31:09 +0530

    Did this question by first taking all elements to get maxOr, then used recurssion by taking & nottaking a particular element of arr, further optimized TC to O(n*maxOr) by using Dp

---
+++ #Leetcode-Daily/2044.cpp
@@ -0,0 +1,45 @@
+// #Question:-
+
+// Link-> https://leetcode.com/problems/count-number-of-maximum-bitwise-or-subsets/description/?envType=daily-question&envId=2024-10-18
+
+// Date-> 18/10/24
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int solve(int idx, int maxOr, vector<int> &nums, int currOr, vector<vector<int>> &dp){
+        if(idx == nums.size()){
+            if(currOr == maxOr){
+                return 1;
+            }
+            return 0;
+        }
+
+        if(dp[idx][currOr] != -1){
+            return dp[idx][currOr];
+        }
+
+        int taking    = solve(idx+1, maxOr, nums, currOr|nums[idx], dp);
+
+        int notTaking = solve(idx+1, maxOr, nums, currOr, dp);
+
+        return dp[idx][currOr] = taking+notTaking; 
+    }
+    int countMaxOrSubsets(vector<int>& nums) {
+        int n = nums.size();
+        int maxOr = 0;
+
+        for(auto &num : nums){
+            maxOr |= num;
+        }
+
+        int currOr = 0;
+        vector<vector<int>> dp(n+1, vector<int>(maxOr+1, -1));
+
+        return solve(0, maxOr, nums, currOr, dp);
+    }
+};
