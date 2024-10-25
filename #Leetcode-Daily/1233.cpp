f21a22f67b31d4decfff81bdfb8fd5ebef0e21ae
Sumit <jha.07sumit2003@gmail.com>
2024-10-25 13:25:36 +0530

    Did this by using sorting & map, check first for presence in map & if not present add it or else break for that loop

---
+++ #Leetcode-Daily/1233.cpp
@@ -0,0 +1,41 @@
+// #Question:-
+
+// Link-> https://leetcode.com/problems/remove-sub-folders-from-the-filesystem/description/?envType=daily-question&envId=2024-10-25
+
+// Date-> 25/10/24
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+        vector<string> res;
+        unordered_map<string, bool> mp;
+        sort(folder.begin(), folder.end(), [](const string &a, const string &b){
+            return a.size()<b.size();
+        });
+
+        for(int i=0; i<folder.size(); i++){
+            string ch = "";
+            bool flag = true;
+            for(int j=0; j<folder[i].size(); j++){
+                if(folder[i][j] == '/' && mp.find(ch) != mp.end()){
+                    flag = false;
+                    break;
+                }
+                ch += folder[i][j];
+            }
+            if(flag && mp.find(ch) == mp.end()){
+                mp.insert({ch,true});
+            }
+        }
+
+        for(auto &x : mp){
+            res.push_back(x.first);
+        }
+        return res;
+    }
+};
