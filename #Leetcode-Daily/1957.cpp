7e9ca0964c17cb1a6ce13bc2336e4b1b934e9ece
Sumit <jha.07sumit2003@gmail.com>
2024-11-01 22:58:53 +0530

    Did this question with creating a new string and adding if less than 3

---
+++ #Leetcode-Daily/1957.cpp
@@ -0,0 +1,24 @@
+// #Question:-
+
+// Link-> https://leetcode.com/problems/delete-characters-to-make-fancy-string/description/?envType=daily-question&envId=2024-11-01
+
+// Date-> 01/11/24
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string makeFancyString(string s) {
+        string ans;
+        ans.reserve(s.size());
+        for (int i = 0; i < s.size(); i++) {
+            if (i < 2 || !(s[i] == s[i - 1] && s[i - 1] == s[i - 2])) {
+                ans += s[i];
+            }
+        }
+        return ans;
+    }
+};
