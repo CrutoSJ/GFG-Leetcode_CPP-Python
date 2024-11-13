f75d1e86bbdcc06a5816721b15de8e9aa862ac1a
Sumit <jha.07sumit2003@gmail.com>
2024-11-13 19:41:35 +0530

    Did this first with brute force - O(n^2), optimized to O(nlogn) by sorting & finding upper & lower bound

---
+++ #Leetcode-Daily/2563.cpp
@@ -0,0 +1,29 @@
+// #Question:-
+
+// Link-> https://leetcode.com/problems/count-the-number-of-fair-pairs/description/?envType=daily-question&envId=2024-11-13
+
+// Date-> 13/11/24
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        long long res = 0;
+
+        for(int i=0; i<n; i++){
+            int idx = lower_bound(nums.begin()+i+1, nums.end(), lower-nums[i])-nums.begin();
+            int x = idx-1-i;
+            idx = upper_bound(nums.begin()+i+1, nums.end(), upper-nums[i])-nums.begin();
+            int y = idx-1-i;
+            res += (y-x);
+        }
+
+        return res;
+    }
+};
