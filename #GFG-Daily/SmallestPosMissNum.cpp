7d8441465e59993d17edccd8b0284123ecfd57af
Sumit <jha.07sumit2003@gmail.com>
2024-11-27 20:33:35 +0530

    Did this with hashing, then optimized to O(1) SC

---
+++ #GFG-Daily/SmallestPosMissNum.cpp
@@ -0,0 +1,34 @@
+// #Question:-
+
+// Link-> https://www.geeksforgeeks.org/problems/smallest-positive-missing-number-1587115621/1
+
+// Date-> 27/11/24
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    // Function to find the smallest positive number missing from the array.
+    int missingNumber(vector<int> &arr) {
+        int n = arr.size();
+        
+        int i = 0;
+        while(i<n){
+            if(arr[i]>0 && arr[i]<=n && arr[i] != arr[arr[i]-1]){
+                swap(arr[i], arr[arr[i]-1]);
+            } else{
+                i++;
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            if(arr[i] != i+1){
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+};
