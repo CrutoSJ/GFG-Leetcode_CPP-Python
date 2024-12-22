23300a4d2c704e113106120dec32dc5ca1f3e834
Sumit <jha.07sumit2003@gmail.com>
2024-12-22 15:44:29 +0530

    Did this question priority queue

---
+++ #Leetcode-Daily/2940.cpp
@@ -0,0 +1,44 @@
+// #Question:-
+
+// Link-> https://leetcode.com/problems/find-building-where-alice-and-bob-can-meet/?envType=daily-question&envId=2024-12-22
+
+// Date-> 22/12/24
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> leftmostBuildingQueries(vector<int>& heights, vector<vector<int>>& queries) {
+        vector<vector<vector<int>>> storeQueries(heights.size());
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> maxIndex;
+        vector<int> result(queries.size(), -1);
+
+        for (int currQuery = 0; currQuery < queries.size(); currQuery++) {
+            int a = queries[currQuery][0], b = queries[currQuery][1];
+            if (a < b && heights[a] < heights[b]) {
+                result[currQuery] = b;
+            } else if (a > b && heights[a] > heights[b]) {
+                result[currQuery] = a;
+            } else if (a == b) {
+                result[currQuery] = a;
+            } else {
+                storeQueries[max(a, b)].push_back(
+                    {max(heights[a], heights[b]), currQuery});
+            }
+        }
+
+        for (int index = 0; index < heights.size(); index++) {
+            while (!maxIndex.empty() && maxIndex.top()[0] < heights[index]) {
+                result[maxIndex.top()[1]] = index;
+                maxIndex.pop();
+            }
+            for (auto& element : storeQueries[index]) {
+                maxIndex.push(element);
+            }
+        }
+        return result;
+    }
+};
