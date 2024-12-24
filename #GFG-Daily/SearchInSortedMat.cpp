a4d254c8ef761ac5e6eba8edfb124ffe7bc82fd0
Sumit <jha.07sumit2003@gmail.com>
2024-12-24 19:58:03 +0530

    Did changes and applied Binary Search to reduce TC to O(log(n*m))

--- #GFG-Daily/SearchInSortedMat.cpp
+++ #GFG-Daily/SearchInSortedMat.cpp
@@ -13,15 +13,19 @@ class Solution {
   public:
     // Function to search a given number in row-column sorted matrix.
     bool searchMatrix(vector<vector<int>> &mat, int x) {
-        int i=0, j=mat[0].size()-1, n=mat.size();
-        while(i<n && j>=0){
-            int temp = mat[i][j];
+        int n=mat.size(), m=mat[0].size();
+        int low=0, high=n*m-1;
+        
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            int temp = mat[mid/m][mid%m];
+            
             if(temp == x){
                 return true;
             } else if(temp < x){
-                i++;
+                low = mid+1;
             } else{
-                j--;
+                high = mid-1;
             }
         }
         return false;
