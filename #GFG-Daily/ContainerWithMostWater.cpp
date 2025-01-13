5d97b47e6be9787ea4d31a9a2dbc7a96d13b877e
Sumit <jha.07sumit2003@gmail.com>
2025-01-13 16:20:36 +0530

    Did this using two-pointer approach

---
+++ #GFG-Daily/ContainerWithMostWater.cpp
@@ -0,0 +1,22 @@
+// #Question:-
+
+// Link-> https://www.geeksforgeeks.org/problems/trapping-rain-water-1587115621/1
+
+// Date-> 13/01/25
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int maxWater(vector<int> &arr) {
+        int i=0, j=arr.size()-1, res=0;
+        while(i<j){
+            res = max(res, (j-i) * min(arr[i],arr[j]));
+            arr[i]<arr[j] ? i++ : j--;
+        }
+        return res;
+    }
+};
