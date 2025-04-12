f84b0617e09e639b4f85a02affd937f3d6c1284e
Sumit <jha.07sumit2003@gmail.com>
2025-04-12 22:27:59 +0530

    Did this que of leetcode

---
+++ #GFG-Daily/FloodFillAlgo.cpp
@@ -0,0 +1,41 @@
+// #Question:-
+
+// Link-> https://www.geeksforgeeks.org/problems/flood-fill-algorithm1856/1
+
+// Date-> 12/04/25
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+      int m, n;
+      vector<vector<int>> directions{{0,1},{0,-1},{1,0},{-1,0}};
+      
+      void dfs(vector<vector<int>>& image, int i, int j, int val,int currColor) {
+          
+          if(i < 0 || i >= m || j < 0 || j >= n || image[i][j] != currColor){
+              return;
+          }
+          image[i][j] = val;
+          
+          for(auto& dir : directions){
+              int x = i + dir[0];
+              int y = j + dir[1];
+              dfs(image,x,y,val,currColor);
+          }
+      }
+      vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
+                                    int newColor) {
+          
+          m = image.size();
+          n = image[0].size();
+          int currColor = image[sr][sc];
+          if(currColor != newColor){
+              dfs(image,sr,sc,newColor,currColor); 
+          }
+          return image;
+      }
+  };
