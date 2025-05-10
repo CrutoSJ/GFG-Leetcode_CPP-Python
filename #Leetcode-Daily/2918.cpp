d0d72f9d10c78f35c290a36211f7d568161b68f7
Sumit <jha.07sumit2003@gmail.com>
2025-05-10 09:55:26 +0530

    Did this que of leetcode

---
+++ #Leetcode-Daily/2918.cpp
@@ -0,0 +1,36 @@
+// #Question:-
+
+// Link-> https://leetcode.com/problems/minimum-equal-sum-of-two-arrays-after-replacing-zeros/description/?envType=daily-question&envId=2025-05-10
+
+// Date-> 10/05/25
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long minSum(vector<int>& nums1, vector<int>& nums2) {
+        long long sum1 = 0, sum2 = 0;
+        long long zero1 = 0, zero2 = 0;
+        for (int i : nums1) {
+            sum1 += i;
+            if (i == 0) {
+                sum1++;
+                zero1++;
+            }
+        }
+        for (int i : nums2) {
+            sum2 += i;
+            if (i == 0) {
+                sum2++;
+                zero2++;
+            }
+        }
+        if (!zero1 && sum2 > sum1 || !zero2 && sum1 > sum2) {
+            return -1;
+        }
+        return max(sum1, sum2);
+    }
+};
