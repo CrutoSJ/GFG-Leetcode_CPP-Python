6b28ab3d26d9c62c6cb2e952f98bcb710ec74794
Sumit <jha.07sumit2003@gmail.com>
2025-05-11 22:00:08 +0530

    Did this que of gfg

---
+++ #GFG-Daily/K-thLargestSumContiSubarray.cpp
@@ -0,0 +1,29 @@
+// #Question:-
+
+// Link-> https://www.geeksforgeeks.org/problems/k-th-largest-sum-contiguous-subarray/1
+
+// Date-> 11/05/25
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int kthLargest(vector<int> &arr, int k) {
+        priority_queue<int,vector<int>,greater<>>p;
+        int n=arr.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            int x=0;
+            for(int j=i;j<n;j++){
+                x+=arr[j];
+                if(p.size()<k) p.push(x);
+                else if(p.top()<x) p.pop(),p.push(x);
+                
+            }
+        }
+        return p.top();
+    }
+};
