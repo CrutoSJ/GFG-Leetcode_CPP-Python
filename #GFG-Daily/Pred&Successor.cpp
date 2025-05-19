d35409214ec8ed391df145e7732edd2c1a4f15c5
Sumit <jha.07sumit2003@gmail.com>
2025-05-19 19:25:27 +0530

    Did this que with BST property

---
+++ #GFG-Daily/Pred&Successor.cpp
@@ -0,0 +1,63 @@
+// #Question:-
+
+// Link-> https://www.geeksforgeeks.org/problems/predecessor-and-successor/1
+
+// Date-> 19/05/25
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+   public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Solution {
+  public:
+  Node* pre=NULL;
+  Node* succ=NULL;
+  int ele1=INT_MIN;
+  int ele2=INT_MAX;
+  void Solve1(Node*root,int key){
+      if(root==NULL)
+      return;
+      if(root->data<key){
+          if(ele1<root->data){
+              ele1=root->data;
+              pre=root;
+          }
+          Solve1(root->right,key);
+      }
+      else
+      Solve1(root->left,key);
+  }
+  void Solve2(Node* root,int key){
+      if(root==NULL)
+      return;
+      if(root->data>key){
+          if(ele2>root->data){
+              ele2=root->data;
+              succ=root;
+          }
+          Solve2(root->left,key);
+      }
+      else
+      Solve2(root->right,key);
+  }
+    vector<Node*> findPreSuc(Node* root, int key) {
+      Solve1(root,key);  
+      Solve2(root,key);  
+        return {pre,succ};
+    }
+};
