89ce09deca5565e98888802c7b2fd4994a1e8eda
Sumit <jha.07sumit2003@gmail.com>
2025-05-19 19:27:04 +0530

    Did sorting and just checked conditions

---
+++ #Leetcode-Daily/3024.cpp
@@ -0,0 +1,26 @@
+// #Question:-
+
+// Link-> https://leetcode.com/problems/type-of-triangle/description/?envType=daily-question&envId=2025-05-19
+
+// Date-> 19/05/25
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        if (nums[0] + nums[1] <= nums[2]) {
+            return "none";
+        } else if (nums[0] == nums[2]) {
+            return "equilateral";
+        } else if (nums[0] == nums[1] || nums[1] == nums[2]) {
+            return "isosceles";
+        } else {
+            return "scalene";
+        }
+    }
+};
