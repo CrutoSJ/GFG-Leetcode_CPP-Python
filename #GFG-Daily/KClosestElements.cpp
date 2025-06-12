62c1fd2c9fd94f845c01e577fbf7443d7db45726
Sumit <jha.07sumit2003@gmail.com>
2025-06-12 19:57:14 +0530

    Did this que using priority queue (max heap)

---
+++ #GFG-Daily/KClosestElements.cpp
@@ -0,0 +1,43 @@
+// #Question:-
+
+// Link-> https://www.geeksforgeeks.org/problems/k-closest-elements3619/1
+
+// Date-> 12/06/25
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class compare {
+public:
+bool operator()(pair<int, int>& a, pair<int, int>& b){
+        if (a.first == b.first)
+        {
+            return a.second < b.second;
+        }
+        return a.first > b.first; 
+    }
+};
+class Solution {
+  public:
+    vector<int> printKClosest(vector<int> arr, int k, int x) {
+        priority_queue<pair<int,int>,vector<pair<int,int>>,compare>pq;
+        for(int i=0;i<arr.size();i++){
+            int a=abs(arr[i]-x);
+            pq.push({a,arr[i]});
+        }
+        vector<int>ans;
+        while(k>0){
+            if(pq.top().first==0){
+                pq.pop();
+            }
+            else{
+            ans.push_back(pq.top().second);
+            pq.pop();
+            k--;
+            }
+        }
+        return ans;
+    }
+};
