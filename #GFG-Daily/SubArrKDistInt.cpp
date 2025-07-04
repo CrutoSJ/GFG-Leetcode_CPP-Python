3d2aeea6d2c7be4c13c3f05e13d652ef48ee8531
Sumit <jha.07sumit2003@gmail.com>
2025-07-04 19:03:39 +0530

    Did this que with hashMap & 2 pointer approach

---
+++ #GFG-Daily/SubArrKDistInt.cpp
@@ -0,0 +1,36 @@
+// #Question:-
+
+// Link-> https://www.geeksforgeeks.org/problems/subarrays-with-at-most-k-distinct-integers/1
+
+// Date-> 04/07/25
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int countAtMostK(vector<int> &arr, int k) {
+        int n=arr.size();
+        unordered_map<int,int>mp;
+        
+        int i=0;
+        int j=0;
+        int count=0;
+        
+        while(j<n){
+            mp[arr[j]]++;
+    
+            while(mp.size() > k){
+                mp[arr[i]]--;
+                if(mp[arr[i]]==0) mp.erase(arr[i]);
+                i++;
+                
+            }
+         count+=(j-i+1);
+         j++;
+        }
+        return count;
+    }
+};
