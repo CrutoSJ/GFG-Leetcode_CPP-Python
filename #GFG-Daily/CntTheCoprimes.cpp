c4d84919de401779142092e47de7d74b711a256a
Sumit <jha.07sumit2003@gmail.com>
2025-07-21 19:47:31 +0530

    Did this question of GFG

---
+++ #GFG-Daily/CntTheCoprimes.cpp
@@ -0,0 +1,38 @@
+// #Question:-
+
+// Link-> https://www.geeksforgeeks.org/problems/count-the-coprimes/1
+
+// Date-> 21/07/25
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int cntCoprime(vector<int>& arr) {
+        int M = *max_element(arr.begin(), arr.end());
+
+        vector<int> freq(M + 1, 0);
+        for (int x : arr) freq[x]++;
+
+        vector<int> multiples(M + 1, 0);
+        for (int i = 1; i <= M; ++i) {
+            for (int j = i; j <= M; j += i) {
+                multiples[i] += freq[j];
+            }
+        }
+
+        vector<long long> dp(M + 1, 0);
+        for (int i = M; i >= 1; --i) {
+            long long c = multiples[i];
+            dp[i] = c * (c - 1) / 2;
+            for (int k = 2 * i; k <= M; k += i) {
+                dp[i] -= dp[k];
+            }
+        }
+        
+        return (int)dp[1];
+    }
+};
