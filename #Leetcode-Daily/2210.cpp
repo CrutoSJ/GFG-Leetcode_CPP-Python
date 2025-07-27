b2dfde1b33d0be2cc45d805ffb68857eee9fccc9
Sumit <jha.07sumit2003@gmail.com>
2025-07-27 22:41:09 +0530

    Did this que of leetcode

---
+++ #Leetcode-Daily/2210.cpp
@@ -0,0 +1,47 @@
+// #Question:-
+
+// Date-> 27/07/25
+
+// Link-> https://leetcode.com/problems/count-hills-and-valleys-in-an-array/?envType=daily-question&envId=2025-07-27
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countHillValley(vector<int>& nums) {
+        int res = 0;
+        int n = nums.size();
+        for (int i = 1; i < n - 1; ++i) {
+            if (nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = 0;
+            for (int j = i - 1; j >= 0; --j) {
+                if (nums[j] > nums[i]) {
+                    left = 1;
+                    break;
+                } else if (nums[j] < nums[i]) {
+                    left = -1;
+                    break;
+                }
+            }
+            int right = 0;
+            for (int j = i + 1; j < n; ++j) {
+                if (nums[j] > nums[i]) {
+                    right = 1;
+                    break;
+                } else if (nums[j] < nums[i]) {
+                    right = -1;
+                    break;
+                }
+            }
+            if (left == right && left != 0) {
+                ++res;
+            }
+        }
+        return res;
+    }
+};
