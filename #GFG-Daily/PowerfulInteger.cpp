38f7a28638f6329ef842175d7116f225b93b19ef
Sumit <jha.07sumit2003@gmail.com>
2025-07-31 21:58:57 +0530

    Did this que with map

---
+++ #GFG-Daily/PowerfulInteger.cpp
@@ -0,0 +1,41 @@
+// #Question:-
+
+// Date-> 31/07/25
+
+// Link-> https://www.geeksforgeeks.org/problems/powerfull-integer--170647/1
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+class Solution {
+  public:
+    int powerfulInteger(vector<vector<int>>& in, int k) {
+        map<ll,ll>mp;
+        for(auto it : in){
+            mp[it[0]]++;
+            mp[it[1]+1]--;
+        }
+        ll h=0;
+        ll ans=-1;
+        ll prev=LLONG_MIN;
+        bool is=false;
+        for(auto it : mp){
+            h+=it.second;
+            if(is){
+                ans=max(prev,it.first-1);
+            }
+            if(h>=k){
+                is=true;
+            }
+            else{
+                is=false;
+            }
+            prev=it.first;
+        }
+        
+        return ans;
+    }
+};
