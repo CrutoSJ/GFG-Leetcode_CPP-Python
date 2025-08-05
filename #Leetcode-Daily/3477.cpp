fcb7c30a2e24c6aec86a2826745bd4e35cbdd96e
Sumit <jha.07sumit2003@gmail.com>
2025-08-05 21:10:48 +0530

    Did this que of simulation

---
+++ #Leetcode-Daily/3477.cpp
@@ -0,0 +1,30 @@
+// #Question:-
+
+// Link-> https://leetcode.com/problems/fruits-into-baskets-ii/description/?envType=daily-question&envId=2025-08-05
+
+// Date-> 05/08/25
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int count = 0;
+        int n = baskets.size();
+        for (auto fruit : fruits) {
+            int unset = 1;
+            for (int i = 0; i < n; i++) {
+                if (fruit <= baskets[i]) {
+                    baskets[i] = 0;
+                    unset = 0;
+                    break;
+                }
+            }
+            count += unset;
+        }
+        return count;
+    }
+};
