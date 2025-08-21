b02a6e1e921a6e602f16310aac60f2ae53f11e05
Sumit <jha.07sumit2003@gmail.com>
2025-08-21 20:04:23 +0530

    Did que of Leetcode Daily

---
+++ #Leetcode-Daily/1504.cpp
@@ -0,0 +1,39 @@
+// #Question:-
+
+// Link-> https://leetcode.com/problems/count-submatrices-with-all-ones/description/?envType=daily-question&envId=2025-08-21
+
+// Date-> 21/08/25
+
+// #Solution:-
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numSubmat(vector<vector<int>>& mat) {
+        int n = mat[0].size();
+        vector<int> heights(n, 0);
+        int res = 0;
+        for (const auto& row : mat) {
+            for (int i = 0; i < n; ++i) {
+                heights[i] = (row[i] == 0) ? 0 : heights[i] + 1;
+            }
+            stack<vector<int>> st;
+            st.push({-1, 0, -1});
+            for (int i = 0; i < n; ++i) {
+                int h = heights[i];
+                while (st.top()[2] >= h) {
+                    st.pop();
+                }
+                auto& top = st.top();
+                int j = top[0];
+                int prev = top[1];
+                int cur = prev + (i - j) * h;
+                st.push({i, cur, h});
+                res += cur;
+            }
+        }
+        return res;
+    }
+};
