c81223b300b186f8957b1f806c683f30e5d4f479
Sumit <108573409+CrutoSJ@users.noreply.github.com>
2025-08-25 21:41:30 +0530

    Did this que with sorting & simulation

---
+++ #GFG-Daily/MaximizeMedianAfterKAddOp.cpp
@@ -0,0 +1,33 @@
+// #Question:-
+
+// Link-> https://www.geeksforgeeks.org/problems/maximize-median-after-doing-k-addition-operation/1
+
+// Date-> 25/08/25
+
+// #Solution:-
+
+class Solution {
+  public:
+    int maximizeMedian(vector<int>& arr, int k) {
+        int n = arr.size(), cur = (n-1)/2;
+        sort(arr.begin(), arr.end());
+        
+        if(n%2 == 0 && arr[cur+1]-arr[cur] >= k) return (arr[cur+1] + arr[cur] + k)/2;
+        
+        int len = 1;
+        while(k) {
+            while(cur < n-1 && arr[cur] == arr[cur+1]) cur++, len++;
+            
+            if(cur == n-1)  return arr[cur] + k / len;
+            
+            int diff = arr[cur+1] - arr[cur];
+            
+            if(k >= diff * len) cur++, k -= diff * len, len++;
+            else return arr[cur] + k / len;
+            
+        }
+        
+        return arr[cur];
+        
+    }
+};
